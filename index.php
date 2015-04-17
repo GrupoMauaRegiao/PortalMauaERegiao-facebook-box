4e72d92c621d942d7c2e62bb58bb4e58774aafd5
marcker <marckfree@gmail.com>
2015-04-17 11:53:56 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="bower_components/normalize.css/normalize.css">
+        <link rel="stylesheet" href="styles/styles.css">
+        <title>Portal Mauá e Região &#8212; Facebook Box</title>
+    </head>
+    <body>
+        <section class="facebook-box-overlay"></section>
+
+        <section class="facebook-box">
+            <div class="close-bar">
+                <div class="close-button"></div>
+            </div>
+
+            <div class="facebook-widget">
+                <div class="fb-page" data-href="https://www.facebook.com/pages/Portal-Mau%C3%A1-e-Regi%C3%A3o/359211450756546" data-width="320" data-height="220" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/Portal-Mau%C3%A1-e-Regi%C3%A3o/359211450756546"><a href="https://www.facebook.com/pages/Portal-Mau%C3%A1-e-Regi%C3%A3o/359211450756546">Portal Mauá e Região</a></blockquote></div></div>
+            </div>
+
+            <a target="_blank" href="https://www.facebook.com/pages/Portal-Mau%C3%A1-e-Regi%C3%A3o/359211450756546">
+                <div class="button-fanpage">
+                    <button>Curta nossa página no facebook</button>
+                </div>
+            </a>
+        </section>
+    </body>
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3&appId=655285364605085";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <script src="scripts/scripts.js"></script>
+</html>
